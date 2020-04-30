5737b5854404175ad413fb13dd0f91ba97d3aa7d
bguiz <bguiz@users.noreply.github.com>
2020-04-30 09:34:05 +0800

    step: 06-03: add tests

--- test/adoption.test.js
+++ test/adoption.test.js
@@ -1,5 +1,42 @@
 const Adoption = artifacts.require('Adoption');
 
-contract('Adoption', (accounts) => {
+const assert = require('assert');
 
+const BN = web3.utils.BN;
+
+contract('Adoption', (accounts) => {
+  it('account can adopt pet', async () => {
+    const inst = await Adoption.deployed();
+
+    const adopterAccount = accounts[5];
+    const expectedPetId = new BN(8);
+    const adoptTxInfo = await inst.adopt(
+      expectedPetId,
+      {
+        from: adopterAccount,
+      },
+    );
+
+    assert.equal(
+      adoptTxInfo.receipt.status,
+      true,
+      'adoption transaction failed',
+    );
+  });
+
+  it('remembers adopter account', async () => {
+    inst = await Adoption.deployed();
+
+    const adopterAccount = accounts[5];
+    const expectedPetId = new BN(8);
+    const returnedAccount = await inst.adopters(
+      expectedPetId,
+    );
+
+    assert.equal(
+      returnedAccount,
+      adopterAccount,
+      'returned adopter account mismatch',
+    );
+  });
 });
